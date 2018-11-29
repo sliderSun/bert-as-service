d3f39b440c57d31a958e5e68f3163796d33866ad
hanhxiao <hanhxiao@tencent.com>
2018-11-29 18:15:40 +0800

    change to bilinear

--- run_classifier_service.py
+++ run_classifier_service.py
@@ -479,7 +479,7 @@ def create_model(is_training, feature, labels, num_labels):
             # I.e., 0.1 dropout
             output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
 
-        logits = linear_logit(output_layer, num_labels)
+        logits = bilinear_logit(output_layer, num_labels)
         # logits = tf.matmul(output_layer, output_weights, transpose_b=True)
         # logits = tf.nn.bias_add(logits, output_bias)
         probabilities = tf.nn.softmax(logits, axis=-1)

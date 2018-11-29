bb3d9a0a8e03c38c00f3792ebdc93b09c6a6fe38
hanhxiao <hanhxiao@tencent.com>
2018-11-29 18:26:19 +0800

    change to bilinear

--- run_classifier_service.py
+++ run_classifier_service.py
@@ -465,21 +465,21 @@ def create_model(is_training, feature, labels, num_labels):
     # instead.
     output_layer = feature
 
-    hidden_size = output_layer.shape[-1].value
+    # hidden_size = output_layer.shape[-1].value
 
-    output_weights = tf.get_variable(
-        "output_weights", [num_labels, hidden_size],
-        initializer=tf.truncated_normal_initializer(stddev=0.02))
-
-    output_bias = tf.get_variable(
-        "output_bias", [num_labels], initializer=tf.zeros_initializer())
+    # output_weights = tf.get_variable(
+    #     "output_weights", [num_labels, hidden_size],
+    #     initializer=tf.truncated_normal_initializer(stddev=0.02))
+    #
+    # output_bias = tf.get_variable(
+    #     "output_bias", [num_labels], initializer=tf.zeros_initializer())
 
     with tf.variable_scope("loss"):
         if is_training:
             # I.e., 0.1 dropout
             output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
 
-        logits = bilinear_logit(output_layer, num_labels)
+        logits = linear_logit(output_layer, num_labels)
         # logits = tf.matmul(output_layer, output_weights, transpose_b=True)
         # logits = tf.nn.bias_add(logits, output_bias)
         probabilities = tf.nn.softmax(logits, axis=-1)

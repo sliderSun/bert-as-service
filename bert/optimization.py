331ab3db9ba94c3f596e5169d53a6465b7e2b7c2
hanhxiao <hanhxiao@tencent.com>
2018-11-30 10:48:42 +0800

    fix bert

--- bert/optimization.py
+++ bert/optimization.py
@@ -67,7 +67,7 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu):
         optimizer = tf.contrib.tpu.CrossShardOptimizer(optimizer)
 
     # tvars = tf.trainable_variables()
-    tvars = [v for v in tf.trainable_variables() if v.name.split('/')[0] == 'bert']
+    tvars = [v for v in tf.trainable_variables() if v.name.split('/')[0] != 'bert']
     tf.logging.info("**** Trainable Variables ****")
     for var in tvars:
         tf.logging.info("  name = %s, shape = %s", var.name, var.shape)

6006ef3dd45e56519fccf3be92c11a08c00c2da5
hanhxiao <hanhxiao@tencent.com>
2018-12-03 09:56:59 +0800

    replace optimizer

--- run_classifier_service.py
+++ run_classifier_service.py
@@ -25,8 +25,9 @@ import os
 import GPUtil
 import tensorflow as tf
 
-from bert import modeling, tokenization, optimization
+from bert import modeling, tokenization
 from gpu_env import MODEL_ID
+from nlp.encode_blocks import LSTM_encode
 from nlp.nn import bilinear_logit
 from service.client import BertClient
 
@@ -469,7 +470,7 @@ def create_model(is_training, feature, labels, num_labels):
     # instead.
     output_layer = feature
 
-    hidden_size = output_layer.shape[-1].value
+    # hidden_size = output_layer.shape[-1].value
 
     # output_weights = tf.get_variable(
     #     "output_weights", [num_labels, hidden_size],
@@ -477,11 +478,11 @@ def create_model(is_training, feature, labels, num_labels):
     #
     # output_bias = tf.get_variable(
     #     "output_bias", [num_labels], initializer=tf.zeros_initializer())
-    def lstm_cell():
-        return tf.nn.rnn_cell.BasicLSTMCell(256)
-
-    stacked_lstm = tf.nn.rnn_cell.MultiRNNCell([lstm_cell() for _ in range(3)])
-    initial_state = state = stacked_lstm.zero_state(output_layer.shape[0].value, tf.float32)
+    # def lstm_cell():
+    #     return tf.nn.rnn_cell.BasicLSTMCell(256)
+    #
+    # stacked_lstm = tf.nn.rnn_cell.MultiRNNCell([lstm_cell() for _ in range(3)])
+    # initial_state = state = stacked_lstm.zero_state(output_layer.shape[0].value, tf.float32)
     with tf.variable_scope("loss"):
         # move pooler outside from modeling.py
         # output_layer = tf.layers.dense(
@@ -489,7 +490,9 @@ def create_model(is_training, feature, labels, num_labels):
         #     hidden_size,
         #     activation=tf.tanh,
         #     kernel_initializer=create_initializer(0.02))
-        output_layer = tf.reduce_max(stacked_lstm(output_layer, initial_state), axis=1)
+        output_layer = tf.reduce_max(LSTM_encode(output_layer,
+                                                 num_layers=3, num_units=256,
+                                                 direction='bidirectional'), axis=1)
 
         logits = bilinear_logit(output_layer, num_labels,
                                 act_fn=tf.tanh,
@@ -553,10 +556,8 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
 
         output_spec = None
         if mode == tf.estimator.ModeKeys.TRAIN:
-
-            train_op = optimization.create_optimizer(
-                total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
-
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.8, beta2=0.999, epsilon=1e-7)
+            train_op = optimizer.minimize(total_loss)
             output_spec = tf.contrib.tpu.TPUEstimatorSpec(
                 mode=mode,
                 loss=total_loss,

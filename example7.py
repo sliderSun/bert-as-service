e54cdbd8cdddd7217f324fc40905d2c76aef4133
hanhxiao <hanhxiao@tencent.com>
2018-12-04 15:32:41 +0800

    fix readme remove duplicated example

---
+++ example7.py
@@ -0,0 +1,9 @@
+twitter_data = '/data/cips/data/lab/data/dataset/training.1600000.processed.noemoticon.csv'
+
+with open(twitter_data) as fp:
+    tmp = fp.readlines()
+    tmp = [v.split(',') for v in tmp]
+    tmp = [(v[0].strip('"'), v[-1].strip('"')) for v in tmp]
+
+print(tmp[-10:])
+print(tmp[:10])

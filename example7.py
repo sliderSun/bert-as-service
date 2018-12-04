3256cea88b72a795c66859fee4f07f23229604a9
hanhxiao <hanhxiao@tencent.com>
2018-12-04 15:49:38 +0800

    fix readme remove duplicated example

--- example7.py
+++ example7.py
@@ -9,11 +9,13 @@ print('loading...')
 with open(twitter_data, 'r', encoding='utf8', errors='ignore') as fp:
     tmp = [v.split(',') for v in fp]
     dataset = [(int(v[0].strip().strip('"')), v[-1].strip().strip('"').strip()) for v in tmp]
+    dataset = [v for v in dataset if v[1]]
 
 print('%d samples loaded' % len(dataset))
 
 subset = random.sample(dataset, num_sample)
 subset_text = [v[1] for v in subset]
+print('min_seq_len: %d' % min(len(v) for v in subset_text))
 print('max_seq_len: %d' % max(len(v) for v in subset_text))
 
 exit()

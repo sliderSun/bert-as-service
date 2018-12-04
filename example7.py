2d8598327809470472c590e48e49819210ee312e
hanhxiao <hanhxiao@tencent.com>
2018-12-04 15:40:02 +0800

    fix readme remove duplicated example

--- example7.py
+++ example7.py
@@ -3,6 +3,8 @@ twitter_data = '/data/cips/data/lab/data/dataset/training.1600000.processed.noem
 print('loading...')
 with open(twitter_data, 'r', encoding='utf8', errors='ignore') as fp:
     tmp = [v.split(',') for v in fp]
-    dataset = [(v[0].strip().strip('"'), v[-1].strip().strip('"')) for v in tmp]
+    dataset = [(int(v[0].strip().strip('"')), v[-1].strip().strip('"')) for v in tmp]
 
 print('%d samples loaded' % len(dataset))
+print(dataset[:10])
+print(dataset[-10:])

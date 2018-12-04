b816795ba83cf51c88aff5e172581ac252f4139c
hanhxiao <hanhxiao@tencent.com>
2018-12-04 15:48:02 +0800

    fix readme remove duplicated example

--- example7.py
+++ example7.py
@@ -1,10 +1,21 @@
+import random
+
+from service.client import BertClient
+
+num_sample = 10000
 twitter_data = '/data/cips/data/lab/data/dataset/training.1600000.processed.noemoticon.csv'
 
 print('loading...')
 with open(twitter_data, 'r', encoding='utf8', errors='ignore') as fp:
     tmp = [v.split(',') for v in fp]
-    dataset = [(int(v[0].strip('\s"')), v[-1].strip('"\n\s')) for v in tmp]
+    dataset = [(int(v[0].strip().strip('"')), v[-1].strip().strip('"').strip()) for v in tmp]
 
 print('%d samples loaded' % len(dataset))
-print(dataset[:10])
-print(dataset[-10:])
+
+subset = random.sample(dataset, num_sample)
+subset_text = [v[1] for v in subset]
+print('max_seq_len: %d' % min(len(v) for v in subset_text))
+
+exit()
+bc = BertClient(port=6000, port_out=6001)
+subset_vec = bc.encode(subset_text)

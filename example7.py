954aa42e10b66846ee246e5c82418aa78a263bb0
hanhxiao <hanhxiao@tencent.com>
2018-12-04 15:35:30 +0800

    fix readme remove duplicated example

--- example7.py
+++ example7.py
@@ -1,8 +1,7 @@
 twitter_data = '/data/cips/data/lab/data/dataset/training.1600000.processed.noemoticon.csv'
 
 with open(twitter_data, 'r', encoding='utf8') as fp:
-    tmp = fp.readlines()
-    tmp = [v.split(',') for v in tmp]
+    tmp = [v.split(',') for v in fp]
     tmp = [(v[0].strip('"'), v[-1].strip('"')) for v in tmp]
 
 print(tmp[-10:])

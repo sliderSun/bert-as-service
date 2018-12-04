749089066a4b0605db1a427ed698322f7113ff8b
hanhxiao <hanhxiao@tencent.com>
2018-12-04 16:21:33 +0800

    fix readme remove duplicated example

--- example7.py
+++ example7.py
@@ -9,7 +9,7 @@ from matplotlib.pyplot import savefig
 
 from service.client import BertClient
 
-num_sample = 10000
+num_sample = 100000
 twitter_data = '/data/cips/data/lab/data/dataset/training.1600000.processed.noemoticon.csv'
 
 print('loading...')
@@ -23,15 +23,16 @@ print('%d samples loaded' % len(dataset))
 subset = random.sample(dataset, num_sample)
 subset_text = [v[1] for v in subset]
 subset_label = [v[0] for v in subset]
+num_label = len(set(subset_label))
 print('min_seq_len: %d' % min(len(v.split()) for v in subset_text))
 print('max_seq_len: %d' % max(len(v.split()) for v in subset_text))
-print('unique label: %d' % len(set(subset_label)))
+print('unique label: %d' % num_label)
 
 bc = BertClient(port=6000, port_out=6001)
 subset_vec = bc.encode(subset_text)
-embeddings = TSNE(n_jobs=4).fit_transform(subset_vec)
+embeddings = TSNE(n_jobs=8).fit_transform(subset_vec)
 vis_x = embeddings[:, 0]
 vis_y = embeddings[:, 1]
-plt.scatter(vis_x, vis_y, c=subset_label, cmap=plt.cm.get_cmap("jet", 10), marker='.')
-plt.colorbar(ticks=range(len(set(subset_label))))
+plt.scatter(vis_x, vis_y, c=subset_label, cmap=plt.cm.get_cmap("jet", num_label), marker='.')
+plt.colorbar(ticks=range(num_label))
 savefig('layer-[%d].png' % (-2), bbox_inches='tight')

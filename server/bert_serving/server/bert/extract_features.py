e8f2dda7576520bbd750ee792e530d3152890ce2
hanhxiao <hanhxiao@tencent.com>
2018-12-11 19:41:28 +0800

    add xla tag to readme

--- server/bert_serving/server/bert/extract_features.py
+++ server/bert_serving/server/bert/extract_features.py
@@ -170,7 +170,7 @@ def model_fn_builder(bert_config, init_checkpoint, use_one_hot_embeddings=False,
             print('\n'.join([n.name for n in tf.get_default_graph().as_graph_def().node]))
 
             tmp_g = tf.graph_util.convert_variables_to_constants(tf.get_default_session(), tmp_g,
-                                                                 [pooled])
+                                                                 ['pooling/truediv'])
             print('after constant: %d' % len(tmp_g.node), flush=True)
             print('after constant: %s' % tmp_g, flush=True)
             print('__removed__', flush=True)
